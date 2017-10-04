acd4391b5f1bdf5070734266b1bd7852247b3515
Miguel López <mkfnx11@gmail.com>
2017-10-03 19:03:37 -0500

    Graph API demo that includes request of the user photos, friends, and friend photos. Graph API demo description in README

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var APP_ID = '596457057161787';
+// var APP_ID = '152354135353469';
+
+
 // Loading dinamically the Facebook SDK
 (function(d, s, id){
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -13,10 +17,10 @@
 // Check the API version you want to use.
 window.fbAsyncInit = function() {
     FB.init({
-      appId : '152354135353469',
+      appId : APP_ID,
       cookie : true,
       xfbml : true,
-      version : 'v1.1'
+      version : 'v2.10'
     });
     // Register a pageview on Facebook Analytics
     FB.AppEvents.logPageView();
@@ -29,18 +33,20 @@ window.fbAsyncInit = function() {
 // Here we're using the Facebook API to check the login status.
 // The 'getLoginStatus' method return a response with 'connected'.
 function checkLoginState() {
+  console.log("checkLoginState");
     FB.getLoginStatus(function(response) {
+      console.log("FB.getLoginStatus response");
         if(response.status === 'connected'){
             fbLogin();
         }
     });
 }
- 
+
  // initialize Account Kit
  AccountKit_OnInteractive = function(){
     AccountKit.init(
       {
-        appId:"152354135353469", 
+        appId:APP_ID,
         state: new Date().toString(),
         version:"v1.1",
         fbAppEventsEnabled:true,
@@ -72,7 +78,7 @@ function smsLogin() {
   var countryCode = document.getElementById("country_code").value;
   var phoneNumber = document.getElementById("phone_number").value;
   AccountKit.login(
-    'PHONE', 
+    'PHONE',
     {countryCode: countryCode, phoneNumber: phoneNumber}, // will use default values if not specified
     loginCallback
   );
@@ -101,12 +107,17 @@ function fbLogout() {
 
 // Facebook login button using the SDK and Graph API
 function fbLogin() {
+  console.log("fbLogin");
   var bt = document.querySelector("#fb-login");
   var logged = document.querySelector("#logged");
 
   // Calling the 'login' method
   FB.login((response)=> {
+    console.log("FB.login response");
       console.log(response);
+
+      setupGraphDemo();
+
       // Success, user is logged
       if (response.status === 'connected') {
           // Change the login button to a logout button
@@ -116,7 +127,7 @@ function fbLogin() {
           // Getting user info
           fetch(`https://graph.facebook.com/me?access_token=${response.authResponse.accessToken}`)
           .then((res)=> {return res.json()})
-          .then((res) => { 
+          .then((res) => {
               // Getting user picture
               const userPic = `https://graph.facebook.com/v2.10/${res.id}/picture?access_token=${response.authResponse.accessToken}`;
               logged.innerHTML = `<br>Welcome, ${res.name} <img src='${userPic}'>`;
@@ -125,5 +136,116 @@ function fbLogin() {
       } else {
       // User is not logged.
       }
-    }, {scope: 'public_profile,email'});
+    }, {scope: 'public_profile,email,user_friends,user_photos'});
+}
+
+function setupGraphDemo() {
+  console.log('setup graph demo');
+  var graphDemoNode = document.getElementById('graph_demo');
+  graphDemoNode.style.display = "initial";
+}
+
+function getFriends() {
+  console.log("get friends");
+
+  FB.api(
+    '/me/friends/',
+    'GET',
+    {
+      "fields":"id,name,picture"
+    },
+    getFriendsCallback(response)
+  );
+}
+
+function getFriendsCallback(response) {
+  if ( response.data ) {
+    var friendsList = document.getElementById('friends_list');
+    document.getElementById('friends_list_node').style.display = "initial";
+    document.getElementById('friend_photos_list_node').style.display = "none";
+    document.getElementById('photos_list_node').style.display = "none";
+
+    while (friendsList.firstChild) {
+      friendsList.removeChild(friendsList.firstChild);
+    }
+
+    parseFriendsResponse(response);
+  }
+}
+
+function parseFriendsResponse(friendsResponse) {
+  console.log("parse friends response");
+
+  var friendsListNode = document.getElementById("friends_list_node");
+  var friendsList = document.getElementById("friends_list");
+
+  for ( var i=0; i<friendsResponse.data.length; i++ ) {
+    var friend = friendsResponse.data[i];
+    var li = document.createElement("li");
+    li.innerHTML =
+    `<div> ${friend.name}
+    <img src='${friend.picture.data.url}'>
+      <a onclick="getFriendPhotos(${friend.id})" class="waves-effect waves-light btn #4267b2 indigo">
+        Get Friend Photos
+      </a>
+    </div>`;
+    friendsList.appendChild(li);
+  }
+}
+
+function getFriendPhotos(friendId) {
+  console.log("get friend photos");
+
+  FB.api(
+    `/${friendId}/photos/`,
+    'GET',
+    { "fields":"id,picture,created_time" },
+    function(response) {
+      console.log("get friends response");
+      console.log(response);
+
+      document.getElementById('friend_photos_list_node').style.display = "initial";
+      var friendPhotosList = document.getElementById("friend_photos_list");
+
+      if ( response.data ) {
+        while (friendPhotosList.firstChild) {
+          friendPhotosList.removeChild(friendPhotosList.firstChild);
+        }
+        parsePhotosResponse(response, friendPhotosList);
+      }
+    }
+  );
+}
+
+function parsePhotosResponse( response, parentNode ) {
+  for ( var i=0; i<response.data.length; i++ ) {
+    var photo = response.data[i];
+    var li = document.createElement("li");
+    li.innerHTML = `<div>${photo.created_time} <img src='${photo.picture}'> </div>`;
+    parentNode.appendChild(li);
+  }
+}
+
+function getPhotos() {
+  console.log("get photos");
+
+  FB.api(
+    '/me/photos',
+    'GET',
+    { "fields":"id,picture,name,created_time" },
+    function(response) {
+      console.log("get photos response");
+      console.log(response);
+
+      var photosList = document.getElementById("photos_list");
+      if ( response.data ) {
+        document.getElementById('friends_list_node').style.display = "none";
+        document.getElementById('photos_list_node').style.display = "initial";
+        while (photosList.firstChild) {
+          photosList.removeChild(photosList.firstChild);
+        }
+        parsePhotosResponse(response, photosList);
+      }
+    }
+  );
 }
